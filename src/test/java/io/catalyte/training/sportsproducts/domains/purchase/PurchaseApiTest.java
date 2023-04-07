3a4959fa4ebb53656916dbd921797462d94ac6b4
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-04-06 21:19:22 -0400

    Resolved merge conflicts with Devins API Integration Tests by merging with my integration test

--- src/test/java/io/catalyte/training/sportsproducts/domains/purchase/PurchaseApiTest.java
+++ src/test/java/io/catalyte/training/sportsproducts/domains/purchase/PurchaseApiTest.java
@@ -1,9 +1,13 @@
 package io.catalyte.training.sportsproducts.domains.purchase;
 
 import static io.catalyte.training.sportsproducts.constants.Paths.PURCHASES_PATH;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.catalyte.training.sportsproducts.domains.product.Product;
 import io.catalyte.training.sportsproducts.domains.product.ProductRepository;
@@ -13,13 +17,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -35,6 +39,13 @@ public class PurchaseApiTest {
 
     private CreditCard testCreditCard = new CreditCard("1234567890123456", "111", "04/30", "Visa");
 
+    private List<BillingAddress> testAddresses;
+    private String[] emails = {"email@address.com", "email@address.net", "email@address.edu"};
+    private Map<String, Integer> purchaseCounts;
+
+    @Autowired
+    public PurchaseRepository purchaseRepository;
+
     @Autowired
     private ProductRepository productRepository;
 
@@ -42,6 +53,7 @@ public class PurchaseApiTest {
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
         setTestPurchase();
+        saveTestPurchasesToRepositoryWithDifferentEmails();
     }
 
     /**
@@ -79,10 +91,39 @@ public class PurchaseApiTest {
         testPurchase.setDeliveryAddress(testDeliveryAddress);
         testPurchase.setCreditCard(testCreditCard);
 
+        testAddresses = new ArrayList<>();
+        purchaseCounts = new HashMap<>();
+        for(String email: emails){
+            Purchase tempPurchase = new Purchase();
+            BillingAddress tempAddress = testBillingAddress;
+            tempAddress.setEmail(email);
+            testAddresses.add(tempAddress);
+            tempPurchase.setBillingAddress(tempAddress);
+            tempPurchase.setDeliveryAddress(testDeliveryAddress);
+            tempPurchase.setCreditCard(testCreditCard);
+            tempPurchase.setProducts(purchases);
+            purchaseRepository.save(tempPurchase);
+            purchaseCounts.put(email, 1);
+        }
+
+    }
+
+    private void saveTestPurchasesToRepositoryWithDifferentEmails() {
+        //assemble some test objects
+
+        //add a purchase so 1 email returns a list of more than 1 purchase
+        Purchase secondPurchase = new Purchase();
+        BillingAddress secondEmail = new BillingAddress();
+        secondEmail.setEmail(emails[0]);
+        secondPurchase.setBillingAddress(secondEmail);
+        purchaseRepository.save(secondPurchase);
+        purchaseCounts.replace(emails[0], purchaseCounts.get(emails[0]) + 1);
+            System.out.println("purchaseRepository.findAll() = " + purchaseRepository.findAll());
     }
 
     @Test
     public void savePurchaseReturns201() throws Exception {
+
         // object mapper for creating a json string
         ObjectMapper mapper = new ObjectMapper();
 
@@ -284,5 +325,61 @@ public class PurchaseApiTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    public void dataBaseFixtureTest() throws Exception {
+        saveTestPurchasesToRepositoryWithDifferentEmails();
+        List<Purchase> testPurchases = purchaseRepository.findAll();
+        System.out.println("testPurchases = " + testPurchases);
+        assertTrue(testPurchases.size() > 0);
+    }
+
+
+    @Test
+    public void findPurchasesByEmailReturnsEmailList() throws Exception {
+
+        System.out.println("purchaseRepository.findAll() = " + purchaseRepository.findAll());
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (String email : emails) {
+            MockHttpServletResponse response = mockMvc.perform(get(PURCHASES_PATH + "/" + email))
+                    .andReturn().getResponse();
+            List<Purchase> purchases = mapper.readValue(response.getContentAsString(),
+                    new TypeReference<List<Purchase>>() {
+                    });
+            assertEquals(Integer.valueOf(purchaseCounts.get(email)), Integer.valueOf(purchases.size()));
+        }
+
+        String purchasesJson =
+                mockMvc.perform(
+                                get(PURCHASES_PATH + "/" + emails[0]))
+                        .andReturn()
+                        .getResponse()
+                        .getContentAsString();
+        Integer numPurchases =
+                mapper.readValue(purchasesJson, new TypeReference<List<Purchase>>() {
+                }).size();
+        assertEquals(Integer.valueOf(2), numPurchases);
+    }
+
+    @Test
+    public void findPurchasesByEmailEmailNotFoundReturnsEmptyList() throws Exception {
+        String purchases = mockMvc.perform(get(PURCHASES_PATH + "/not@anEmail.com"))
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("[]", purchases);
+    }
+
+    @Test
+    public void findPurchasesByEmailEmailNotFoundReturnsOk() throws Exception {
+        mockMvc.perform(get(PURCHASES_PATH + "/not@anEmail.com"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getAllPurchasesReturns404() throws Exception {
+        mockMvc.perform(get(PURCHASES_PATH))
+                .andExpect(status().is(404));
     }
 }
